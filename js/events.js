2ca5ef819a7d4da8c73813e294090e0218e020b6
Ting Chung <tchung682@gmail.com>
2020-05-15 17:11:05 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,17 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $(document).on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
 });
