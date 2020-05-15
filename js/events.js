13ebe1e35d574798156d7f5c408240f80d063d84
Ting Chung <tchung682@gmail.com>
2020-05-15 17:31:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function pressIt(){
   })
 }
 function submitIt(){
-  $('#submit').on("submit", function(){
+  $(document).on("submit", function(){
     alert("Your form is going to be submitted now.")
   })
 }
