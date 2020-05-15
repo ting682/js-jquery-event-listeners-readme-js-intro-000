7ce2771380c841bd052f9aab4c4b32e9d1c7e99e
Ting Chung <tchung682@gmail.com>
2020-05-15 17:32:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $(document).on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71){
+      alert('G was pressed')
+    }
+  })
+}
+function submitIt(){
+  $(document).on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
